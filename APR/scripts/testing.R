4397054a57b5c8720d85233b3d1b735ef282e259
Thomas Boughen <b9031732@ncl.ac.uk>
2024-05-31 22:42:13 +0100

    Update APR

--- APR/scripts/testing.R
+++ APR/scripts/testing.R
@@ -1,4 +1,24 @@
 
 # -------------------------------------------------------------------------
+library(plot3D)
+library(ggplot2)
+library(ggallin)
+res=readRDS('../data/mcmc.outputs/dir_out.rds')
 
+
+res2 = res[,2]$res
+res2 = res2[-(1:1e4),]
+library(MASS)
+library(fishualize)
+n=635
+pal <- viridis_pal()(n)
+dens = kde2d(res2$xi, res2$sig)
+filled.contour(dens,col=pal,nlevels = 400)
+
+
+
+ggplot(res2[,c('xi','sig')], aes(x=xi, y=sig,fill = ..level..)) +stat_density_2d(geom = "polygon")
+
+
+# plot(density(df[df$from=='BAsim',]$xi))
 
